df005a93fda2f17192d97908eb5b2b7ffa93d15b
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-12-26 21:22:44 +0530

    Update and rename Majority Element(Moore's Voting Algorithms) to Majority Element(Moore's Voting Algorithms).java

---
+++ Searching/Majority Element(Moore's Voting Algorithms).java
@@ -0,0 +1,40 @@
+class Majority
+{
+    static int findCandidate(int a[], int size)
+    {
+    int maj_index = 0, count = 1;
+    for (int i = 1; i < size; i++) {
+        if (a[maj_index] == a[i])
+            count++;
+        else
+            count--;
+        if (count == 0) {
+            maj_index = i;
+            count = 1;
+        }
+    }
+    return a[maj_index];
+    }
+    static boolean isMajority(int a[], int size, int cand)
+    {
+    int count = 0;
+    for (int i = 0; i < size; i++)
+
+        if (a[i] == cand)
+            count++;
+
+    if (count > size / 2)
+        return true;
+
+    else
+        return false;
+}
+    static int majorityElement(int a[], int size)
+    {
+        int candidate=findCandidate(a, size);
+         if (isMajority(a, size, candidate))
+         return candidate;
+        else
+        return -1;
+    }
+    }
