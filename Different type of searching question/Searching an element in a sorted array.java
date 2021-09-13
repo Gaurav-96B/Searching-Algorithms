24cd72d17164748f617ebc17400cf69ba239aa8b
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-09-13 08:22:28 +0530

    Create Searching an element in a sorted array.java

---
+++ Different type of searching question/Searching an element in a sorted array.java
@@ -0,0 +1,22 @@
+static int searchInSorted(int arr[], int N, int K)
+    {
+        int start=0;
+        int end=N-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==K)
+            {
+                return 1;
+            }
+            else if(K>arr[mid])
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
